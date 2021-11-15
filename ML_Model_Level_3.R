9780365b8e3ab5b3c3bd0dc19e0afbdd357b5446
Frank Mathews <63658905+fmathews11@users.noreply.github.com>
2021-11-15 14:13:50 -0500

    Add files via upload

--- ML_Model_Level_3.R
+++ ML_Model_Level_3.R
@@ -5,6 +5,7 @@ library(readr)
 library(finetune)
 library(vip)
 library(ggthemes)
+library(SHAPforxgboost)
 set.seed(20175)
 dd <- read.csv("https://raw.githubusercontent.com/fmathews11/College_Football_Stats/main/Allplayedgames.csv")
 
@@ -17,120 +18,96 @@ dd<- dd%>%
   mutate(Neutral_Location = as.factor(case_when(Neutral_Location == FALSE ~ 0, TRUE ~1)))%>%
   mutate(Team_FBS = as.factor(Team_FBS))%>%
   mutate(Opp_FBS = as.factor(Opp_FBS))%>%
-  mutate(Home = as.factor(Home))
+  mutate(Home = as.factor(Home))%>%
+  na.omit()
 
-dd_split <- initial_split(dd)
+nextweeksgames <- read.csv('https://raw.githubusercontent.com/fmathews11/College_Football_Stats/main/next_weeks_games.csv')
+
+nextweeksgames <- nextweeksgames%>%
+  mutate(Team_FBS = as.factor(Team_FBS))%>%
+  mutate(Opp_FBS = as.factor(Opp_FBS))%>%
+  mutate(Home = as.factor(Home))%>%
+  mutate(Neutral_Location = case_when(
+    Neutral_Location == TRUE ~1,Neutral_Location == FALSE ~0
+  ))%>%
+  mutate(Neutral_Location = as.factor(Neutral_Location))
+
+
+
+dd_split <- initial_split(dd,strata = Result)
 dd_train <- training(dd_split)
 dd_test <- testing(dd_split)
 
-#LEAVING THIS HERE FOR NOW _ COME BACK TO IT LATER...
+# GLMNET
 
-svm_spec <- svm_linear()%>%
-  set_engine("LiblineaR")%>%
-  set_mode("classification")
-
-show_engines('svm_linear')
-
-svm_recipe <- recipe(data = dd_train, formula = Result ~ Home + Neutral_Location + Team_FBS + Opp_FBS + ELO + Opp_ELO)%>%
-  step_dummy(all_nominal_predictors())%>%
-  step_normalize(all_numeric_predictors())
-
-svm_recipe %>% prep()
-
-svm_workflow <- workflow()%>%
-  add_recipe(svm_recipe)%>%
-  add_model(svm_spec)
-
-
-svm_folds <- vfold_cv(dd_train,v = 10, strata = Result)
-
-
-svm_fit <- fit_resamples(svm_workflow,resamples = svm_folds)
-
-svm_fit%>% collect_Metrics()
-
-
-cfbfastR::cfbd_calendar()
-
-
-# KKNN MODEL
-
-kknn_recipe <- 
-  recipe(formula = Result ~ Home + Neutral_Location + Team_FBS + Opp_FBS + ELO + 
-           Opp_ELO, data = dd_train) %>% 
-  step_novel(all_nominal_predictors()) %>% 
-  step_dummy(all_nominal_predictors()) %>% 
+glmnet_recipe <- 
+  recipe(formula = Result ~ Home + Neutral_Location + ELO + Opp_ELO + Team_FBS + Opp_FBS, data = dd_train) %>% 
+  step_novel(all_nominal(), -all_outcomes()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>% 
   step_zv(all_predictors()) %>% 
-  step_normalize(all_numeric_predictors()) 
+  step_normalize(all_predictors(), -all_nominal()) 
 
-kknn_spec <- 
-  nearest_neighbor(neighbors = tune(), weight_func = tune()) %>% 
+juiced <- glmnet_recipe%>%prep()%>%juice()
+
+glmnet_spec <- 
+  logistic_reg(penalty = tune(), mixture = tune()) %>% 
   set_mode("classification") %>% 
-  set_engine("kknn") 
+  set_engine("glmnet") 
 
-kknn_workflow <- 
+glmnet_workflow <- 
   workflow() %>% 
-  add_recipe(kknn_recipe) %>% 
-  add_model(kknn_spec) 
+  add_recipe(glmnet_recipe) %>% 
+  add_model(glmnet_spec) 
 
-knn_folds <- vfold_cv(dd_train,v = 10, strata = Result)
+glmnet_grid <- tidyr::crossing(penalty = 10^seq(-6, -1, length.out = 20), mixture = c(0.05, 
+                                                                                      0.2, 0.4, 0.6, 0.8, 1)) 
+
+glm_folds <- vfold_cv(dd_train,v = 10,strata = Result)
+
+glmnet_tune <- 
+  tune_grid(glmnet_workflow, resamples = glm_folds, grid = glmnet_grid)
 
 
-kknn_tune <-
-  tune_race_anova(kknn_workflow, resamples = knn_folds, grid = 10,metrics = metric_set(roc_auc,accuracy),control = control_race(
-    verbose = TRUE,allow_par = TRUE
-  ) )
+glmnet_tune%>%
+  show_best('accuracy')
 
-plot_race(kknn_tune)
+glmnet_best <- glmnet_tune%>%
+  select_best('accuracy')
 
-kknn_tune%>%autoplot()
+final_glmnet_wf <- finalize_workflow(glmnet_workflow,glmnet_best)
 
-show_best()
+final_fit <- final_glmnet_wf%>%
+  last_fit(dd_split)
 
-finalize_workflow(kknn_workflow,select_best(kknn_tune))%>%
-  last_fit(dd_split)%>%
+final_fit%>%
   collect_metrics()
 
-# NeuralNetwork
-
-install.packages("keras")
-
-nn_recipe <-recipe(formula = Result ~ Home + Neutral_Location + Team_FBS + Opp_FBS + ELO + 
-                     Opp_ELO, data = dd_train) %>% 
-  step_novel(all_nominal_predictors()) %>% 
-  step_dummy(all_nominal_predictors()) %>% 
-  step_zv(all_predictors()) %>% 
-  step_normalize(all_numeric_predictors())
-
-nn_recipe%>%prep()
-
-nn_spec <- mlp(hidden_units = tune(),
-               penalty = tune(),
-               dropout = 0,
-               epochs = 500,
-               activation = tune())%>%
-  set_engine('keras')%>%
-  set_mode('classification')
-
-  nn_workflow <- workflow()%>%
-    add_recipe(nn_recipe)%>%
-    add_model(nn_spec)
-
-  nn_folds <- vfold_cv(dd_train, times = 10,strata = Result)  
-
-nn_tune <- tune_race_anova(nn_workflow,resamples = nn_folds,grid = 10,control = control_race(
-  verbose = TRUE
-),
-metrics = metric_set(roc_auc,accuracy))  
+model_fit <- final_glmnet_wf%>%
+  fit(dd_train)
 
 
-#### XGBOOST
+nextweeksgames$Team_Win_Probability <- predict(model_fit,nextweeksgames,type = 'prob')$.pred_1
+
+nextweeksgames%>%
+  group_by(game_id)%>%
+  arrange(-Team_Win_Probability)%>%
+  select(Team,Opponent,Team_Win_Probability)%>%
+  View()
+
+
+
+
+### Spread
+
+hist(dd_train$ELO)
+hist(dd_train$Opp_ELO)
+hist(dd_train$Spread)
 
 xgboost_recipe <- 
-  recipe(formula = Result ~   Home + Neutral_Location +  Team_FBS +
+  recipe(formula = Spread ~   Home + Neutral_Location +  Team_FBS +
            Opp_FBS + ELO + Opp_ELO, data = dd_train) %>% 
-  step_novel(all_nominal(), -all_outcomes()) %>% 
-  step_dummy(all_nominal(), -all_outcomes(), one_hot = TRUE) %>% 
+  step_novel(all_nominal_predictors()) %>% 
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
   step_zv(all_predictors())%>%
   step_normalize(all_numeric_predictors())
 
@@ -139,10 +116,10 @@ juiced<-xgboost_recipe%>%prep()%>%juice()
 num_cores <- parallel::detectCores()
 
 xgboost_spec <- 
-  boost_tree(trees = 2000, min_n = tune(), tree_depth = tune(), learn_rate = tune(), 
+  boost_tree(trees = tune(), min_n = tune(), tree_depth = tune(), learn_rate = tune(), 
              loss_reduction = tune(), sample_size = tune(),stop_iter = 20) %>% 
-  set_mode("classification") %>% 
-  set_engine("xgboost",n.threads = num_cores,eval_metric = "auc") 
+  set_mode("regression") %>% 
+  set_engine("xgboost",eval_metric = "rmse") 
 
 xgboost_workflow <- 
   workflow() %>% 
@@ -152,59 +129,74 @@ xgboost_workflow <-
 
 xgb_folds <- vfold_cv(data = dd_train,v = 10,strata = Result)
 
+doParallel::registerDoParallel(cores = num_cores)
+
 xgboost_tune <-
   tune_race_anova(xgboost_workflow, resamples = xgb_folds, grid = 10,control = control_race(
     verbose = TRUE,
     allow_par = TRUE),
-    metrics = metric_set(roc_auc,accuracy))
+    metrics = metric_set(rmse,rsq))
 
-xgboost_tune%>%show_best('accuracy')
+xgboost_tune%>%
+  collect_metrics()
 
-xgb_best <- xgboost_tune%>%
-  select_best(metric = 'accuracy')
+xgboost_tune%>%
+  show_best('rmse')
 
-final_xgb <- finalize_workflow(xgboost_workflow,xgb_best)
+xgboost_tune%>%
+  show_best('rsq')
 
-final_fit <- final_xgb%>%
+plot_race(xgboost_tune)
+
+xgboost_best <- xgboost_tune%>%
+  select_best('rmse')
+
+xgb_final_wf <- finalize_workflow(xgboost_workflow,xgboost_best)
+
+final_fit <- xgb_final_wf%>%
   last_fit(dd_split)
+
 
 final_fit%>%
   collect_metrics()
 
-preds <- final_fit%>%
-  collect_predictions()
-
-caret::confusionMatrix(preds$.pred_class,preds$Result, positive = "1")
-
-finalize_model(xgboost_spec,xgb_best)%>%
+finalize_model(xgboost_spec,xgboost_best)%>%
   set_engine('xgboost',importance = 'impurity')%>%
-  fit(formula =  Result ~ .,data = juiced)%>%
-  vip(num_features = 35)
+  fit(formula =  Spread ~ .,data = juiced)%>%
+  vip()
 
 raw_xgb <- final_fit%>%
   extract_fit_engine()
 
+
 raw_xgb$evaluation_log%>%
-  ggplot(aes(x = iter,y = training_auc))+
+  ggplot(aes(x = iter,y = training_rmse))+
   geom_line(color='hotpink',size = 1.2)+
   labs(x = 'Boosting Iterations',
-       y = 'Model AUC')+
+       y = 'Model RMSE')+
   theme_hc()
 
-xgb.plot.tree(model = raw_xgb,trees = 100)
+model_fit <- xgb_final_wf%>%
+  fit(dd_train)
 
-xgb.ggplot.shap.summary(data = as.matrix(juiced[,-3]),model = raw_xgb)
+saveRDS(xgb_final_wf,'xgb_spread')
 
-fit_model <- fit(final_xgb,data = dd_train)
-
-nextweeksgames <- read.csv('next_weeks_games.csv')
+nextweeksgames$predicted_spread <- round(predict(model_fit,nextweeksgames)$.pred,digits = 0)
 
 nextweeksgames <- nextweeksgames%>%
-  mutate(Team_FBS = as.factor(Team_FBS))%>%
-  mutate(Opp_FBS = as.factor(Opp_FBS))%>%
-  mutate(Home = as.factor(Home))%>%
-  mutate(Neutral_Location = as.factor(Neutral_Location))
+  arrange(-Team_Win_Probability)%>%
+  mutate(Team_Win_Probability = round(Team_Win_Probability,digits = 2))%>%
+  rename("Predicted_Win_Margin" = 'predicted_spread')
 
-predict_classprob.model_fit(final_xgb,nextweeksgames)
+nextweeksgames%>%
+  select(Team,Opponent,Team_Win_Probability,Predicted_Win_Margin)%>%View()
+  
 
-nextweeksgames$prediction = preds
+
+
+write.csv(nextweeksgames,'week8predictions')
+
+
+
+
+
